568930787ef77f9cde8d21792b4843c6683af977
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-30 21:09:48 +0300

    workarounds

--- app/adapters/stat.js
+++ app/adapters/stat.js
@@ -1,11 +1,34 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
 
 export default DS.RESTAdapter.extend({
   host: 'http://stats.waze.su/by',
   corsWithCredentials: true,
-    namespace: '',
-    buildURL: function() {
-        var normalURL = this._super.apply(this, arguments);
-        return normalURL += '.php', normalURL.replace('stats.php', 'data.php');
+  namespace: '',
+  buildURL: function() {
+      var normalURL = this._super.apply(this, arguments);
+      return normalURL += '.php', normalURL.replace('stats.php', 'data.php');
+  },
+
+  _ajaxRequest(options) {
+    const success = options.success;
+
+    options.success = function(payload, textStatus, jqXHR) {
+      this.preProcess(payload, textStatus, jqXHR, options);
+      return success.call(this, payload, textStatus, jqXHR);
     }
+
+    return this._super(options)
+  },
+
+  preProcess(payload, textStatus, jqXHR, options) {
+    const optionsId = +get(options, 'data.id');
+    if (payload.stats instanceof Array &&  optionsId && get(options, 'data.a') === 'city') {
+      payload.stats.forEach((i) => i.cityId = optionsId);
+    }
+  }
 });
--- app/serializers/stat.js
+++ app/serializers/stat.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
+var i = 1;
+
 export default DS.RESTSerializer.extend({
-  primaryKey: 'time'
+  primaryKey: 'time',
+
+  normalize(type, hash) {
+    var s = this._super(type, hash);
+    s.data.id = hash.cityId + hash.time;
+    return s;
+  }
 });
