d403e8a5e1604ccee7f8280bda264b6f0668663f
Anton <ixxvivxxi@yahoo.com>
2022-11-01 23:21:41 +0300

    feat: move index charts to components

---
+++ app/components/index-chart.js
@@ -0,0 +1,75 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { all } from 'rsvp';
+import { task } from 'ember-concurrency';
+import { CITIES } from 'waze-stats/config';
+
+const DEFAULT_CHARTDATA = {
+  datasets: [
+    {
+      label: '',
+      data: [1],
+      backgroundColor: ['#838383'],
+    },
+  ],
+  labels: ['нет данных'],
+};
+
+export default class IndexChartComponent extends Component {
+  @service('data')
+  dataService;
+
+  @tracked
+  citiesData = [];
+
+  @tracked isPie = true;
+
+  get period() {
+    if (this.args.period) {
+      return this.args.period;
+    }
+
+    return 1;
+  }
+
+  get chartData() {
+    if (this.citiesData.length === 0) {
+      return DEFAULT_CHARTDATA;
+    }
+
+    const data = this.citiesData.sortBy('value').reverse();
+
+    return {
+      datasets: [
+        {
+          label: '',
+          data: data.map((item) => item.value),
+          backgroundColor: data.map((item) => item.color),
+        },
+      ],
+      labels: data.map((item) => item.name),
+    };
+  }
+
+  fetchData = task(this, async () => {
+    const citiesData = await all(
+      CITIES.map((city) =>
+        this.dataService.fetchData
+          .perform(city.id, this.period)
+          .then((data) => {
+            const value =
+              this.period === 1
+                ? data[data.length - 1].online
+                : Math.max(...data.map((item) => item.online));
+            return {
+              ...city,
+              value,
+            };
+          })
+      )
+    );
+
+    this.citiesData = citiesData.filter((item) => item.value);
+  });
+}
