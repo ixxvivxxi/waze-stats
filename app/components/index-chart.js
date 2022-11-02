7f0a3d4fe277a6b1f53e28700d0eca4155541f79
Anton <ixxvivxxi@yahoo.com>
2022-11-02 11:24:27 +0300

    feat: auto update data in index charts

--- app/components/index-chart.js
+++ app/components/index-chart.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { all } from 'rsvp';
 import { task } from 'ember-concurrency';
@@ -19,6 +20,8 @@ const DEFAULT_CHARTDATA = {
 export default class IndexChartComponent extends Component {
   @service('data')
   dataService;
+
+  interval = null;
 
   @tracked
   citiesData = [];
@@ -91,4 +94,18 @@ export default class IndexChartComponent extends Component {
 
     this.citiesData = citiesData.filter((item) => item.value);
   });
+
+  @action
+  async onInit() {
+    await this.fetchData.perform();
+
+    if (this.args.autoUpdate) {
+      this.interval = setInterval(() => this.fetchData.perform(), 600000);
+    }
+  }
+
+  @action
+  onDestroy() {
+    clearInterval(this.interval);
+  }
 }
