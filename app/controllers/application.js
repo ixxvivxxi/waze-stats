b9fcfaf7f840f83d87dd936fd08988e345564531
Anton <ixxvivxxi@yahoo.com>
2022-11-04 21:29:14 +0300

    feat: refactor menu and sidebar with tailwind styles

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,6 @@
 import Controller from '@ember/controller';
-import $ from 'jquery';
 import { CITIES } from 'waze-stats/config';
 
 export default Controller.extend({
   cities: CITIES,
-  
-  actions: {
-    toogleSidebar() {
-      $('.ui.sidebar').sidebar('toggle');
-    },
-  },
 });
